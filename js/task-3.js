33917903bcf3f164b5533bf93d0b80b189133b2d
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-12-20 17:34:32 +0200

    Додає вирiшення завдання 2

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      }
+
+      reject(transaction.id);
+    }, delay);
+  });
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later`);
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
